31d2f5e5b3e765cee295cd27da96437203207564
Asmae MALOULI <18185@ecam.be>
2021-02-22 14:15:46 +0100

    Add bike class

---
+++ App_bovélo/Bovelo_SuperApp/Bike.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bovelo_SuperApp
+{
+    class Bike
+    {
+        public string color;
+        public string type;
+        public string size;
+        public Client client;
+        //public delivery_time ;
+
+        public Bike(string c,string t,string s )
+        {
+            this.color = c;
+            this.type = t;
+            this.size = s;
+            
+
+        }
+        public void SetClient(Client CL)
+        {
+            this.client = CL;
+        }
+
+
+
+
+    }
+}

e922ce5dedfca8a6c80f46c12235df672a9fdf58
Olivia Kinnen <32852709+okinnen@users.noreply.github.com>
2021-03-08 19:12:52 +0100

    Update MounterControl.Designer.cs
    buttons which delete from the data base the mounted bikes AB #483

--- App_bovélo/Bovelo_SuperApp/MounterControl.Designer.cs
+++ App_bovélo/Bovelo_SuperApp/MounterControl.Designer.cs
@@ -1,4 +1,4 @@
-﻿
+
 namespace Bovelo_SuperApp
 {
     partial class MounterControl
@@ -40,12 +40,14 @@ namespace Bovelo_SuperApp
             this.pnl_week_orders_forMounter = new System.Windows.Forms.FlowLayoutPanel();
             this.btn_GetBike_mounters = new System.Windows.Forms.Button();
             this.title_production_planning = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.pnl_titles_week_orders.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnl_titles_week_orders
             // 
             this.pnl_titles_week_orders.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnl_titles_week_orders.Controls.Add(this.label7);
             this.pnl_titles_week_orders.Controls.Add(this.label1);
             this.pnl_titles_week_orders.Controls.Add(this.label5);
             this.pnl_titles_week_orders.Controls.Add(this.label6);
@@ -75,7 +77,7 @@ namespace Bovelo_SuperApp
             this.label5.AutoSize = true;
             this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(959, -1);
+            this.label5.Location = new System.Drawing.Point(802, -1);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(94, 39);
             this.label5.TabIndex = 2;
@@ -86,29 +88,30 @@ namespace Bovelo_SuperApp
             this.label6.AutoSize = true;
             this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(580, -1);
+            this.label6.Location = new System.Drawing.Point(535, -1);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(234, 39);
+            this.label6.Size = new System.Drawing.Size(145, 39);
             this.label6.TabIndex = 6;
-            this.label6.Text = "business name";
+            this.label6.Text = "business";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(366, 2);
+            this.label3.Location = new System.Drawing.Point(344, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(189, 39);
+            this.label3.Size = new System.Drawing.Size(100, 39);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Client name";
+            this.label3.Text = "Client";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(259, -1);
+            this.label8.Location = new System.Drawing.Point(245, -1);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(80, 39);
             this.label8.TabIndex = 3;
@@ -119,7 +122,7 @@ namespace Bovelo_SuperApp
             this.label2.AutoSize = true;
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(853, -1);
+            this.label2.Location = new System.Drawing.Point(700, -1);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(79, 39);
             this.label2.TabIndex = 0;
@@ -168,6 +171,17 @@ namespace Bovelo_SuperApp
             this.title_production_planning.TabIndex = 8;
             this.title_production_planning.Text = "Welcome";
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F);
+            this.label7.Location = new System.Drawing.Point(931, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 37);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Statut";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
             // MounterControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -194,9 +208,10 @@ namespace Bovelo_SuperApp
         public System.Windows.Forms.Label label8;
         public System.Windows.Forms.Label label2;
         public System.Windows.Forms.Label label4;
-        private System.Windows.Forms.FlowLayoutPanel pnl_week_orders_forMounter;
         private System.Windows.Forms.Button btn_GetBike_mounters;
         public System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label title_production_planning;
+        private System.Windows.Forms.Label label7;
+        public System.Windows.Forms.FlowLayoutPanel pnl_week_orders_forMounter;
     }
 }

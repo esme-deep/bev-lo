bd4440f0a09acfbd07f6187163ffe856cd214c30
esme-deep <sam.azr2014@gmail.com>
2021-03-20 12:28:21 +0100

    place of a buttun changed

--- App_bovélo/Bovelo_SuperApp/production_planning.Designer.cs
+++ App_bovélo/Bovelo_SuperApp/production_planning.Designer.cs
@@ -52,9 +52,9 @@ namespace Bovelo_SuperApp
             // 
             this.title_production_planning.AutoSize = true;
             this.title_production_planning.Font = new System.Drawing.Font("Microsoft YaHei", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.title_production_planning.Location = new System.Drawing.Point(477, 18);
+            this.title_production_planning.Location = new System.Drawing.Point(537, 22);
             this.title_production_planning.Name = "title_production_planning";
-            this.title_production_planning.Size = new System.Drawing.Size(111, 30);
+            this.title_production_planning.Size = new System.Drawing.Size(162, 41);
             this.title_production_planning.TabIndex = 0;
             this.title_production_planning.Text = "Welcome";
             this.title_production_planning.Click += new System.EventHandler(this.title_production_planning_Click);
@@ -62,10 +62,10 @@ namespace Bovelo_SuperApp
             // btn_get_orders
             // 
             this.btn_get_orders.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_get_orders.Location = new System.Drawing.Point(477, 305);
+            this.btn_get_orders.Location = new System.Drawing.Point(537, 381);
             this.btn_get_orders.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_get_orders.Name = "btn_get_orders";
-            this.btn_get_orders.Size = new System.Drawing.Size(141, 54);
+            this.btn_get_orders.Size = new System.Drawing.Size(159, 68);
             this.btn_get_orders.TabIndex = 1;
             this.btn_get_orders.Text = "Get this week orders";
             this.btn_get_orders.UseVisualStyleBackColor = true;
@@ -80,10 +80,10 @@ namespace Bovelo_SuperApp
             this.pnl_titles_week_orders.Controls.Add(this.label8);
             this.pnl_titles_week_orders.Controls.Add(this.label2);
             this.pnl_titles_week_orders.Controls.Add(this.label4);
-            this.pnl_titles_week_orders.Location = new System.Drawing.Point(3, 70);
-            this.pnl_titles_week_orders.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pnl_titles_week_orders.Location = new System.Drawing.Point(3, 88);
+            this.pnl_titles_week_orders.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.pnl_titles_week_orders.Name = "pnl_titles_week_orders";
-            this.pnl_titles_week_orders.Size = new System.Drawing.Size(1093, 40);
+            this.pnl_titles_week_orders.Size = new System.Drawing.Size(1229, 50);
             this.pnl_titles_week_orders.TabIndex = 3;
             // 
             // label5
@@ -91,9 +91,9 @@ namespace Bovelo_SuperApp
             this.label5.AutoSize = true;
             this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(884, 1);
+            this.label5.Location = new System.Drawing.Point(994, 1);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(75, 32);
+            this.label5.Size = new System.Drawing.Size(111, 46);
             this.label5.TabIndex = 2;
             this.label5.Text = "order";
             // 
@@ -102,9 +102,9 @@ namespace Bovelo_SuperApp
             this.label6.AutoSize = true;
             this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(500, 2);
+            this.label6.Location = new System.Drawing.Point(562, 2);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(187, 32);
+            this.label6.Size = new System.Drawing.Size(278, 46);
             this.label6.TabIndex = 6;
             this.label6.Text = "business name";
             // 
@@ -113,9 +113,9 @@ namespace Bovelo_SuperApp
             this.label3.AutoSize = true;
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(280, 2);
+            this.label3.Location = new System.Drawing.Point(315, 2);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(152, 32);
+            this.label3.Size = new System.Drawing.Size(226, 46);
             this.label3.TabIndex = 5;
             this.label3.Text = "Client name";
             // 
@@ -124,9 +124,9 @@ namespace Bovelo_SuperApp
             this.label8.AutoSize = true;
             this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(179, 2);
+            this.label8.Location = new System.Drawing.Point(201, 2);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(65, 32);
+            this.label8.Size = new System.Drawing.Size(96, 46);
             this.label8.TabIndex = 3;
             this.label8.Text = "Size";
             // 
@@ -135,9 +135,9 @@ namespace Bovelo_SuperApp
             this.label2.AutoSize = true;
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(731, 0);
+            this.label2.Location = new System.Drawing.Point(856, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 32);
+            this.label2.Size = new System.Drawing.Size(96, 46);
             this.label2.TabIndex = 0;
             this.label2.Text = "time";
             // 
@@ -148,38 +148,63 @@ namespace Bovelo_SuperApp
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(3, -1);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(85, 32);
+            this.label4.Size = new System.Drawing.Size(126, 46);
             this.label4.TabIndex = 1;
             this.label4.Text = "Model";
             // 
             // pnl_week_orders
             // 
             this.pnl_week_orders.AutoScroll = true;
-            this.pnl_week_orders.Location = new System.Drawing.Point(5, 110);
+            this.pnl_week_orders.Location = new System.Drawing.Point(6, 138);
             this.pnl_week_orders.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pnl_week_orders.Name = "pnl_week_orders";
-            this.pnl_week_orders.Size = new System.Drawing.Size(1077, 249);
+            this.pnl_week_orders.Size = new System.Drawing.Size(1212, 311);
             this.pnl_week_orders.TabIndex = 4;
             this.pnl_week_orders.Visible = false;
             // 
             // btn_set_mounters
             // 
-            this.btn_set_mounters.Location = new System.Drawing.Point(469, 390);
+            this.btn_set_mounters.Location = new System.Drawing.Point(528, 488);
             this.btn_set_mounters.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_set_mounters.Name = "btn_set_mounters";
-            this.btn_set_mounters.Size = new System.Drawing.Size(160, 52);
+            this.btn_set_mounters.Size = new System.Drawing.Size(180, 65);
             this.btn_set_mounters.TabIndex = 6;
             this.btn_set_mounters.Text = "Confirm";
             this.btn_set_mounters.UseVisualStyleBackColor = true;
             this.btn_set_mounters.Click += new System.EventHandler(this.btn_set_mounters_Click);
-            
-            
-            
-            
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 29);
+            this.button1.TabIndex = 3;
+            // 
+            // TimeMounter3
+            // 
+            this.TimeMounter3.Location = new System.Drawing.Point(0, 0);
+            this.TimeMounter3.Name = "TimeMounter3";
+            this.TimeMounter3.Size = new System.Drawing.Size(112, 29);
+            this.TimeMounter3.TabIndex = 2;
+            // 
+            // TimeMounter2
+            // 
+            this.TimeMounter2.Location = new System.Drawing.Point(0, 0);
+            this.TimeMounter2.Name = "TimeMounter2";
+            this.TimeMounter2.Size = new System.Drawing.Size(112, 29);
+            this.TimeMounter2.TabIndex = 1;
+            // 
+            // TimeMounter1
+            // 
+            this.TimeMounter1.Location = new System.Drawing.Point(0, 0);
+            this.TimeMounter1.Name = "TimeMounter1";
+            this.TimeMounter1.Size = new System.Drawing.Size(112, 29);
+            this.TimeMounter1.TabIndex = 0;
             // 
             // production_planning
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.TimeMounter1);
             this.Controls.Add(this.TimeMounter2);
@@ -192,7 +217,7 @@ namespace Bovelo_SuperApp
             this.Controls.Add(this.title_production_planning);
             this.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
             this.Name = "production_planning";
-            this.Size = new System.Drawing.Size(1083, 537);
+            this.Size = new System.Drawing.Size(1218, 671);
             this.pnl_titles_week_orders.ResumeLayout(false);
             this.pnl_titles_week_orders.PerformLayout();
             this.ResumeLayout(false);
